c24008c15146c94a0843914f288367ce1050dd7c
Abdoulkarim <17a.karim@gmail.com>
2025-06-12 19:38:28 +0100

    Completed project section

--- script.js
+++ script.js
@@ -24,3 +24,42 @@ const observer = new IntersectionObserver(observerCallback, observerOptions);
 
 // 4. Start observing
 observer.observe(techStack);
+
+/*-------Import project Json file and render ---------*/
+const projectsContainer = document.querySelector(".projects-container");
+
+function renderProjects(data) {
+  data.forEach((project) => {
+    const html = `
+    <div class="project">
+        <img class="project-img" src="${project.projectImg}">
+
+        <div class="project-detail">
+          <h3>${project.projectName}</h3>
+          <p>${project.projectStack}</p>
+        </div>
+
+        <div class="dev-project-link video-link-btn">
+          Go To Video
+        </div>
+    </div>
+    `;
+
+    projectsContainer.insertAdjacentHTML("beforeend", html);
+    console.log(project.projectStack);
+  });
+}
+
+async function fetchProjectData() {
+  try {
+    const response = await fetch("./projects.json");
+    if (!response.ok) throw new Error("Could not fetch data");
+    const data = await response.json();
+    // Render projects
+    renderProjects(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchProjectData();
